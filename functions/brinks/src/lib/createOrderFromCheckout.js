17c330a0ae71df9edb3fd73faf40da5fb285927f
Brett Slaski <brett@thatconference.com>
2021-02-08 12:11:53 -0600

    Adds ability to support isBulkPurchase by line item which auto-alocates ticket if not bulk. package bumps

--- functions/brinks/src/lib/createOrderFromCheckout.js
+++ functions/brinks/src/lib/createOrderFromCheckout.js
@@ -7,24 +7,33 @@ export default function createOrderFromCheckout({ checkoutSession, products }) {
   dlog('createOrderFromCheckout called id: %s', checkoutSession.id);
 
   const { metadata } = checkoutSession;
-  const { memberId, eventId } = metadata;
-  const checkoutLineItems = checkoutSession.line_items.data;
+  const {
+    memberId,
+    eventId,
+    checkoutLineItems: metadataCoLineItmes,
+  } = metadata;
+  const coLineItems = JSON.parse(metadataCoLineItmes);
+  dlog('coLineItems::%o', coLineItems);
+  const checkoutSessionLineItems = checkoutSession.line_items.data;
 
   // build line items off of checkout session lineitem list pulling in product
   // match verifications done in verification section
-  const lineItems = checkoutLineItems.map(stripeLi => {
+  const lineItems = checkoutSessionLineItems.map(stripeLi => {
     const product = products.find(
       p => p.processor.itemRefId === stripeLi.price.id,
     );
+    const coLineItem = coLineItems.find(co => co.productId === product.id);
     return {
       product: product.id,
       quantity: stripeLi.quantity,
       unitPrice: stripeLi.price.unit_amount / 100,
       totalAmount: stripeLi.amount_total / 100,
       stripeProductId: stripeLi.price.product,
+      isBulkPurchase: coLineItem.isBulkPurchase,
     };
   });
 
+  // save discount code information
   const { breakdown } = checkoutSession.total_details;
   const discounts = breakdown
     ? breakdown.discounts.map(d => ({
--- functions/brinks/src/lib/generateAllocationsFromOrder.js
+++ functions/brinks/src/lib/generateAllocationsFromOrder.js
@@ -11,17 +11,23 @@ export default function generateAllocationsFromOrder({
   const { lineItems } = order;
   const orderAllocations = [];
 
+  // create an allocation for each quantity of each line item
   lineItems.forEach(lineItem => {
     const qty = lineItem.quantity;
+    const { isBulkPurchase } = lineItem;
+    dlog(`line item isBulkPurchase: ${isBulkPurchase}`);
     for (let i = 0; i < qty; i += 1) {
       // note, we don't have an order id yet
       const allocation = {
         event: order.event,
         purchasedBy: order.member,
         product: lineItem.product,
-        isClaimed: false,
+        isAllocated: !isBulkPurchase,
+        allocatedTo: isBulkPurchase ? null : order.member,
+        hasCheckedIn: false,
         createdAt: orderCreatedAt,
         lastUpdatedAt: orderCreatedAt,
+        lastUpdatedBy: order.member,
       };
       orderAllocations.push(allocation);
     }
@@ -29,3 +35,10 @@ export default function generateAllocationsFromOrder({
 
   return orderAllocations;
 }
+
+/*
+ * isBulkPurchase
+ * when false the purchase is for the individual
+ * and will be auto-alocated to them.
+ * Quantity validations have already been done before this call
+ */
