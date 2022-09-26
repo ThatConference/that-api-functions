a33403312021ea2067a0cf5de3872f29c12cbe93
Brett Slaski <brett@thatconference.com>
2022-09-26 12:18:55 -0500

    refactor order to allow for better querying

--- functions/brinks/src/lib/createOrderFromCheckout.js
+++ functions/brinks/src/lib/createOrderFromCheckout.js
@@ -70,6 +70,12 @@ export default function createOrderFromCheckout({ checkoutSession, products }) {
     status: 'COMPLETE',
     orderType: 'REGULAR',
     affiliateCode: checkoutSession?.metadata?.affiliateCode ?? null,
+    usedPromotionCodes: discounts.map(d => d.promotionCode),
+    hasProducts: [...new Set(lineItems.map(li => li.product))],
+    hasProductTypes: [...new Set(lineItems.map(li => li.productType))],
+    hasStripeProducts: [...new Set(lineItems.map(li => li.stripeProductId))],
+    hasUiReferences: [...new Set(lineItems.map(li => li.uiReference))],
+    hasBulkPurchase: lineItems.filter(li => li.isBulkPurchase === true) > 0,
   };
 
   return newOrder;
